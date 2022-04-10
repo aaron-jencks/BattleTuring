b71470eb8719092c020ae2c1b5593a6a84e7b5dd
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-04-10 00:46:37 -0500

    finished initial draft of webserver

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import asyncio
 import websockets
+import websockets.server as wserver
 import json
-import time
 
 from parsing.bufferio import StringContainer
 from parsing.commander import Commander
@@ -30,36 +30,71 @@ halt;
 """
 
 
-async def hello(websocket, path):
-    while True:
-        name = await websocket.recv()
-        jobj = json.loads(name)
-        print("< {}".format(name))
+async def hello(websocket: wserver.WebSocketServerProtocol, path):
+    keep_going = True
+    data_q = asyncio.Queue(10)
+    stop_q = asyncio.Queue(10)
 
-        tape = TheTape()
-        machine = TuringMachine(tape)
+    async def io_loop(q: asyncio.Queue, sq: asyncio.Queue):
+        while True:
+            name = await websocket.recv()
+            obj = json.loads(name)
+            print("< {}".format(name))
+            if 'stop' in obj:
+                await q.put(False)
+            else:
+                await q.put(obj)
 
-        if 'initial_string' in jobj and len(jobj['initial_string']) > 0:
-            tape.initialize_tape(jobj['initial_string'])
+    async def main_loop(q: asyncio.Queue, sq: asyncio.Queue):
+        while True:
+            try:
+                jobj = await q.get()
+                print(jobj)
+                if jobj:
+                    tape = TheTape()
+                    machine = TuringMachine(tape)
 
-        if 'code' in jobj and len(jobj['code']) > 0:
-            code = jobj['code']
-        else:
-            code = test
+                    if 'initial_string' in jobj and len(jobj['initial_string']) > 0:
+                        tape.initialize_tape(jobj['initial_string'])
 
-        tape.reset(True)
+                    if 'code' in jobj and len(jobj['code']) > 0:
+                        code = jobj['code']
+                    else:
+                        code = test
 
-        code = AST(Lexer(StringContainer(code)))
-        code.build_tree()
+                    tape.reset(True)
 
-        commander = Commander(code)
-        while commander.has_next():
-            commander.run_next(machine)
-            jobj = machine.to_dict()
-            jobj['last'] = False
-            await websocket.send(json.dumps(jobj))
-        jobj['last'] = True
-        await websocket.send(json.dumps(jobj))
+                    code = AST(Lexer(StringContainer(code)))
+                    code.build_tree()
+
+                    commander = Commander(code)
+                    sobj = {}
+                    while commander.has_next():
+                        try:
+                            qobj = q.get_nowait()
+                            if not qobj:
+                                print('Halting execution')
+                                if isinstance(qobj, dict):
+                                    await q.put(qobj)
+                                break
+                        except asyncio.QueueEmpty:
+                            pass
+
+                        commander.run_next(machine)
+                        sobj = machine.to_dict()
+                        sobj['last'] = False
+                        print('> {}'.format(json.dumps(sobj)))
+                        await websocket.send(json.dumps(sobj))
+                    sobj['last'] = True
+                    print('> {}'.format(json.dumps(sobj)))
+                    await websocket.send(json.dumps(sobj))
+            except asyncio.QueueEmpty:
+                continue
+
+    await asyncio.gather(
+        io_loop(data_q, stop_q),
+        main_loop(data_q, stop_q)
+    )
 
 
 if __name__ == '__main__':
